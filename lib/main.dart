9fbbde4f029026f1c4ae4366a4db2ca906026f29
Tri Nanda <2001020024@student.umrah.ac.id>
2024-10-26 08:24:35 +0700

    Penyelesaian Code Lab 1

--- lib/main.dart
+++ lib/main.dart
@@ -123,62 +123,147 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+ 
+// void main() => runApp(const MyApp());
+ 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const FirstScreen(),// Panggil FirstScreen di sini
+ 
+//     );
+//   }
+// }
+ 
+// class FirstScreen extends StatelessWidget {
+//   const FirstScreen({super.key});
+ 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Screen'),
+//         actions: <Widget>[
+//           IconButton(
+//             icon: const Icon(
+//               Icons.search,
+//               color: Colors.blue,
+//             ),
+//             onPressed: () {
+              
+//             },
+//           ),
+//         ],
+//         leading: IconButton(
+//           icon: const Icon(
+//             Icons.menu,
+//             color: Colors.blue,
+//           ),
+//           onPressed: () {
+            
+//           },
+//         ),
+//       ),
+//       body: const Center(
+//           child: Text('Hello world'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }
+
+// Code Lab 1
 import 'package:flutter/material.dart';
- 
+
+
 void main() => runApp(const MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstScreen(),// Panggil FirstScreen di sini
- 
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
     );
   }
 }
- 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
- 
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.search,
-              color: Colors.blue,
+      body: SafeArea(child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 16.0),
+            child: const Text('Farm House Lembang',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
             ),
-            onPressed: () {
-              
-            },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16.0),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.calendar_today),
+                    SizedBox(height: 8.0),
+                    Text('Open Everyday'),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.access_time),
+                    SizedBox(height: 8.0),
+                    Text('09.00 - 20:00')
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.monetization_on),
+                    SizedBox(height: 8.0),
+                    Text('25.000'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: const Text(
+              'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0
+                ),
+              ),
           ),
         ],
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.blue,
-          ),
-          onPressed: () {
-            
-          },
-        ),
       ),
-      body: const Center(
-          child: Text('Hello world'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
+      ) 
     );
   }
 }
